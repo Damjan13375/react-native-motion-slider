04a879888c5008da126f80137475365d73fbbdc1
Damjan <dichoski.damjan@alite-international.com>
2020-11-24 03:39:44 +0100

    rotate

--- src/index.js
+++ src/index.js
@@ -1,366 +1,418 @@
 /**
  * @author Vitor Silva - https://github.com/VitorCodes
  */
-import React, { Component } from 'react';
-import { View, Text, Dimensions, Animated, Easing, PanResponder } from 'react-native';
-import Style from './styles';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  Dimensions,
+  Animated,
+  Easing,
+  PanResponder,
+} from "react-native";
+import Style from "./styles";
 
-const { 
-	width: Width = width, 
-	height: Height = height 
-} = Dimensions.get('window');
+const { width: Width = width, height: Height = height } = Dimensions.get(
+  "window"
+);
 
 class Slider extends Component {
-	static defaultProps = {
-		width: Math.floor(Width * 0.85),
-		height: Math.floor(Height * 0.07),
-		borderRadius: 10,
-		backgroundColor: ['#2196F3'],
-		decimalPlaces: 0,
-		title: '',
-		titleColor: null,
-		titleStyle: {},
-		min: 0,
-		max: 50,
-		value: 100,
-		units: '',
-		minColor: '#fff',
-		maxColor: '#fff',
-		valueColor: null,
-		valueBackgroundColor: '#fff',
-		fontSize: 10,
-		fontWeight: 'normal',
-		fontFamily: null,
-		onValueChanged: () => null,
-		onPressIn: () => null,
-		onPressOut: () => null,
-		onDrag: () => null,
-	};
+  static defaultProps = {
+    width: Math.floor(Width * 0.85),
+    height: Math.floor(Height * 0.07),
+    borderRadius: 10,
+    backgroundColor: ["#2196F3"],
+    decimalPlaces: 0,
+    title: "",
+    titleColor: null,
+    titleStyle: {},
+    min: 0,
+    max: 50,
+    value: 100,
+    units: "",
+    minColor: "#fff",
+    maxColor: "#fff",
+    valueColor: null,
+    valueBackgroundColor: "#fff",
+    fontSize: 10,
+    fontWeight: "normal",
+    fontFamily: null,
+    onValueChanged: () => null,
+    onPressIn: () => null,
+    onPressOut: () => null,
+    onDrag: () => null,
+  };
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		let {
-			// Bar 
-			width, height, borderRadius, backgroundColor,
-			// Min
-			minColor,
-			// Max
-			maxColor,
-			// Value
-			value,
-			valueBackgroundColor,
-			decimalPlaces,
-			// Min, Max and Value
-			fontSize, fontFamily, fontWeight,
-		} = props;
-		
-		this.titleStyle = {
-			marginHorizontal: Math.floor(width * 0.05),
-			marginVertical: Math.floor(height * 0.3),
-		};
+    let {
+      // Bar
+      width,
+      height,
+      borderRadius,
+      backgroundColor,
+      // Min
+      minColor,
+      // Max
+      maxColor,
+      // Value
+      value,
+      valueBackgroundColor,
+      decimalPlaces,
+      // Min, Max and Value
+      fontSize,
+      fontFamily,
+      fontWeight,
+    } = props;
 
-		this.barStyle = {
-			width,
-			height,
-			borderRadius,
-			backgroundColor
-		};
+    this.titleStyle = {
+      marginHorizontal: Math.floor(width * 0.05),
+      marginVertical: Math.floor(height * 0.3),
+    };
 
-		this.minStyle = {
-			color: minColor,
-			fontSize,
-			fontFamily,
-			fontWeight,
-			width: height,
-			marginHorizontal: borderRadius,
-		};
+    this.barStyle = {
+      width,
+      height,
+      borderRadius,
+      backgroundColor,
+    };
 
-		this.maxStyle = {
-			color: maxColor,
-			fontSize,
-			fontFamily,
-			fontWeight,
-			width: height,
-			marginHorizontal: borderRadius
-		};
+    this.minStyle = {
+      color: minColor,
+      fontSize,
+      fontFamily,
+      fontWeight,
+      width: height,
+      marginHorizontal: borderRadius,
+    };
 
-		this.valueStyle = {
-			fontSize,
-			fontFamily,
-			fontWeight
-		};
+    this.maxStyle = {
+      color: maxColor,
+      fontSize,
+      fontFamily,
+      fontWeight,
+      width: height,
+      marginHorizontal: borderRadius,
+    };
 
-		this.valueOuterContainerStyle = {
-			backgroundColor,
-			width: height,
-			height,
-			borderRadius: Math.floor(height / 2),
-		};
+    this.valueStyle = {
+      fontSize,
+      fontFamily,
+      fontWeight,
+    };
 
-		this.valueInnerContainerStyle = {
-			backgroundColor: valueBackgroundColor,
-			width: height - Math.floor(height * 0.2),
-			height: height - Math.floor(height * 0.2),
-			borderRadius: Math.floor((height - Math.floor(height * 0.2)) / 2),
-		};
+    this.valueOuterContainerStyle = {
+      backgroundColor,
+      width: height,
+      height,
+      borderRadius: Math.floor(height / 2),
+    };
 
-		this.minX = borderRadius + Math.floor(height / 2);
-		this.maxX = width - borderRadius - Math.floor(height / 2);
-		this.panResponder = this.setPanResponder();
-		this.gradient = this.setGradient();
+    this.valueInnerContainerStyle = {
+      backgroundColor: valueBackgroundColor,
+      width: height - Math.floor(height * 0.2),
+      height: height - Math.floor(height * 0.2),
+      borderRadius: Math.floor((height - Math.floor(height * 0.2)) / 2),
+    };
 
-		this.state = {
-			value: value.toFixed(decimalPlaces),
-			animTranslateY: new Animated.Value(0),
-			animScale: new Animated.Value(1),
-			posX: this.xOfValue(value),
-			pressed: false
-		};
-	}
+    this.minX = borderRadius + Math.floor(height / 2);
+    this.maxX = width - borderRadius - Math.floor(height / 2);
+    this.panResponder = this.setPanResponder();
+    this.gradient = this.setGradient();
 
-	xOfValue(y) {
-		let { max, min } = this.props;
-		let m = Number(((min - max) / (this.minX - this.maxX)).toFixed(20));
-		let x = (y - min) / m;
+    this.state = {
+      value: value.toFixed(decimalPlaces),
+      animTranslateY: new Animated.Value(0),
+      animScale: new Animated.Value(1),
+      posX: this.xOfValue(value),
+      pressed: false,
+    };
+  }
 
-		return Math.floor(x + this.minX);
-	}
+  xOfValue(y) {
+    let { max, min } = this.props;
+    let m = Number(((min - max) / (this.minX - this.maxX)).toFixed(20));
+    let x = (y - min) / m;
 
-	calculateValue(posX) {
-		let { max, min, decimalPlaces, onDrag, onValueChanged } = this.props;
+    return Math.floor(x + this.minX);
+  }
 
-		if(posX < this.minX) posX = this.minX;
-		if(posX > this.maxX) posX = this.maxX;
-		
-		let m = Number(((min - max) / (this.minX - this.maxX)).toFixed(20));
-		let x = posX;
-		let value = ((m * x) - (this.minX * m) + min).toFixed(decimalPlaces);
+  calculateValue(posX) {
+    let { max, min, decimalPlaces, onDrag, onValueChanged } = this.props;
 
-		this.setState({ value });
-		onDrag();
-		onValueChanged(value);
-	}
+    if (posX < this.minX) posX = this.minX;
+    if (posX > this.maxX) posX = this.maxX;
 
-	setGradient() {
-		let { backgroundColor, min, max } = this.props;
+    let m = Number(((min - max) / (this.minX - this.maxX)).toFixed(20));
+    let x = posX;
+    let value = (m * x - this.minX * m + min).toFixed(decimalPlaces);
 
-		// Has only one color -> outputRange will use the same values
-		if(backgroundColor.length == 1) {
-			return {
-				inputRange: [min, max],
-				outputRange: [backgroundColor[0], backgroundColor[0]]
-			};
-		};
+    this.setState({ value });
+    onDrag();
+    onValueChanged(value);
+  }
 
-		// Has two or more colors -> defines the output range with backgroundColor values and calculates middleRange values for inputRange
-		let gradient =  {
-			inputRange: [min, max],
-			outputRange: backgroundColor
-		};
+  setGradient() {
+    let { backgroundColor, min, max } = this.props;
 
-		let currentRange = min;
-		let middleRanges = [];
+    // Has only one color -> outputRange will use the same values
+    if (backgroundColor.length == 1) {
+      return {
+        inputRange: [min, max],
+        outputRange: [backgroundColor[0], backgroundColor[0]],
+      };
+    }
 
-		for(var i = 0; i < backgroundColor.length - 2; i++) {
-			middleRanges.push(currentRange + Math.floor((1 / backgroundColor.length) * 100));
-			currentRange += Math.floor((1 / backgroundColor.length) * 100);
-		}
+    // Has two or more colors -> defines the output range with backgroundColor values and calculates middleRange values for inputRange
+    let gradient = {
+      inputRange: [min, max],
+      outputRange: backgroundColor,
+    };
 
-		gradient.inputRange.splice(1, 0, ...middleRanges);
-		return gradient;
-	}
+    let currentRange = min;
+    let middleRanges = [];
 
-	setPanResponder() {
-		return PanResponder.create({
-			onStartShouldSetPanResponder: (evt, gestureState) => true,
-			onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-			onMoveShouldSetPanResponder: (evt, gestureState) => true,
-			onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-			onPanResponderTerminationRequest: (evt, gestureState) => true,
-			onPanResponderGrant: (evt, gestureState) => {
-				let posX = Math.floor(evt.nativeEvent.locationX);
-				this.calculateValue(posX);
-				this.onPressIn(posX);
-			},
-			onPanResponderMove: (evt, gestureState) => {
-				let posX = Math.floor(evt.nativeEvent.locationX);
-				let posY = Math.floor(evt.nativeEvent.locationY);
+    for (var i = 0; i < backgroundColor.length - 2; i++) {
+      middleRanges.push(
+        currentRange + Math.floor((1 / backgroundColor.length) * 100)
+      );
+      currentRange += Math.floor((1 / backgroundColor.length) * 100);
+    }
 
-				if(posY < 0 || posY > this.props.height) return;
+    gradient.inputRange.splice(1, 0, ...middleRanges);
+    return gradient;
+  }
 
-				this.calculateValue(posX);
-				this.setState({ posX });
-			},
-			onPanResponderRelease: (evt, gestureState) => {
-				this.onPressOut();
-			},
-			onPanResponderTerminate: (evt, gestureState) => {
-				this.onPressOut();
-			},
-		});
-	}
+  setPanResponder() {
+    return PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderGrant: (evt, gestureState) => {
+        let posX = Math.floor(evt.nativeEvent.locationX);
+        this.calculateValue(posX);
+        this.onPressIn(posX);
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        let posX = Math.floor(evt.nativeEvent.locationX);
+        let posY = Math.floor(evt.nativeEvent.locationY);
 
-	onPressIn(posX) {
-		let { height, onPressIn, onValueChanged } = this.props;
-		let { animScale, animTranslateY, pressed, value } = this.state;
+        if (posY < 0 || posY > this.props.height) return;
 
-		if(pressed) return;
+        this.calculateValue(posX);
+        this.setState({ posX });
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        this.onPressOut();
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        this.onPressOut();
+      },
+    });
+  }
 
-		this.setState({
-			pressed: true,
-			posX
-		}, () => {
-			Animated.parallel([
-				Animated.timing(animScale, {
-					toValue: 1.4,
-					duration: 200,
-				}),
-				Animated.spring(animTranslateY, {
-					toValue: -(height - Math.floor(height * 0.3)),
-					bounciness: 12,
-				})
-			]).start();
+  onPressIn(posX) {
+    let { height, onPressIn, onValueChanged } = this.props;
+    let { animScale, animTranslateY, pressed, value } = this.state;
 
-			onPressIn();
-			onValueChanged(value);
-		});
-	}
+    if (pressed) return;
 
-	onPressOut() {
-		let { animScale, animTranslateY, pressed, value } = this.state;
-		let { onPressOut, onValueChanged } = this.props;
+    this.setState(
+      {
+        pressed: true,
+        posX,
+      },
+      () => {
+        Animated.parallel([
+          Animated.timing(animScale, {
+            toValue: 1.4,
+            duration: 200,
+          }),
+          Animated.spring(animTranslateY, {
+            toValue: -(height - Math.floor(height * 0.3)),
+            bounciness: 12,
+          }),
+        ]).start();
 
-		if(!pressed) return;
+        onPressIn();
+        onValueChanged(value);
+      }
+    );
+  }
 
-		this.setState({
-			pressed: false
-		}, () => {
-			Animated.parallel([
-				Animated.timing(animScale, {
-					toValue: 1,
-					duration: 300,
-				}),
-				Animated.timing(animTranslateY, {
-					toValue: 0,
-					duration: 300,
-					easing: Easing.out(Easing.exp),
-				}),
-			]).start();
+  onPressOut() {
+    let { animScale, animTranslateY, pressed, value } = this.state;
+    let { onPressOut, onValueChanged } = this.props;
 
-			onPressOut();
-			onValueChanged(value);
-		});
-	}
+    if (!pressed) return;
 
-	getCurrentColor() {
-		return new Animated.Value(Number(this.state.value)).interpolate({
-			inputRange: this.gradient.inputRange,
-			outputRange: this.gradient.outputRange
-		});
-	}
+    this.setState(
+      {
+        pressed: false,
+      },
+      () => {
+        Animated.parallel([
+          Animated.timing(animScale, {
+            toValue: 1,
+            duration: 300,
+          }),
+          Animated.timing(animTranslateY, {
+            toValue: 0,
+            duration: 300,
+            easing: Easing.out(Easing.exp),
+          }),
+        ]).start();
 
-	barAnimationStyle() {
-		let { value, animScale } = this.state;
-		
-		return {
-			transform: [{  
-				scale: animScale.interpolate({ 
-					inputRange: [1, 1.4],
-					outputRange: [0.9, 1] 
-				})
-			}],
-			backgroundColor: this.getCurrentColor()
-		};
-	}
+        onPressOut();
+        onValueChanged(value);
+      }
+    );
+  }
 
-	titleAnimationStyle() {
-		let { animScale } = this.state;
-		let { titleColor } = this.props;
-		
-		return {
-			opacity: animScale.interpolate({ 
-				inputRange: [1, 1.1, 1.4],
-				outputRange: [1, 0.2, 0] 
-			}),
-			color: titleColor || this.getCurrentColor()
-		};
-	}
+  getCurrentColor() {
+    return new Animated.Value(Number(this.state.value)).interpolate({
+      inputRange: this.gradient.inputRange,
+      outputRange: this.gradient.outputRange,
+    });
+  }
 
-	renderTitle() {
-		let { title, titleStyle } = this.props;
+  barAnimationStyle() {
+    let { value, animScale } = this.state;
 
-		if(!title && title.length === 0) return;
+    return {
+      transform: [
+        {
+          scale: animScale.interpolate({
+            inputRange: [1, 1.4],
+            outputRange: [0.9, 1],
+          }),
+        },
+      ],
+      backgroundColor: this.getCurrentColor(),
+    };
+  }
 
-		return (
-			<Animated.Text style={[
-				Style.title, 
-				this.titleStyle, 
-				titleStyle,
-				this.titleAnimationStyle()
-			]}>
-				{title}
-			</Animated.Text>
-		);
-	}
+  titleAnimationStyle() {
+    let { animScale } = this.state;
+    let { titleColor } = this.props;
 
-	renderValue() {
-		let { value, posX, animTranslateY, animScale } = this.state;
-		let { width, height, borderRadius, units } = this.props;
-		let left = posX - Math.floor(height / 2);
+    return {
+      opacity: animScale.interpolate({
+        inputRange: [1, 1.1, 1.4],
+        outputRange: [1, 0.2, 0],
+      }),
+      color: titleColor || this.getCurrentColor(),
+    };
+  }
 
-		if(left <= borderRadius) {
-			left = borderRadius;
-		}
+  renderTitle() {
+    let { title, titleStyle } = this.props;
 
-		if(left >= width - borderRadius - height) {
-			left = width - borderRadius - height;
-		}
+    if (!title && title.length === 0) return;
 
-		return (
-			<Animated.View style={[
-				Style.valueContainer, 
-				{ 
-					left,
-					transform: [{ scale: animScale }],
-				}
-			]}>
-				<Animated.View 
-					style={[
-						Style.valueOuterContainer,
-						this.valueOuterContainerStyle,
-						{ 
-							transform: [{ translateY: animTranslateY }],
-							backgroundColor: this.getCurrentColor()
-						}
-					]}>
-					<View style={[Style.valueInnerContainer, this.valueInnerContainerStyle]}>
-						<Animated.Text style={[Style.value, this.valueStyle, { color: this.getCurrentColor() }]} numberOfLines={1}>{`${value}${units}`}</Animated.Text>
-					</View>
-				</Animated.View>
-			</Animated.View>
-		);
-	}
+    return (
+      <Animated.Text
+        style={[
+          Style.title,
+          this.titleStyle,
+          titleStyle,
+          this.titleAnimationStyle(),
+        ]}
+      >
+        {title}
+      </Animated.Text>
+    );
+  }
 
-  	render() {
-		let { min, max, decimalPlaces, units } = this.props;
+  renderValue() {
+    let { value, posX, animTranslateY, animScale } = this.state;
+    let { width, height, borderRadius, units } = this.props;
+    let left = posX - Math.floor(height / 2);
 
-		return (
-			<View>
-				{this.renderTitle()}
-				<Animated.View style={[
-					Style.bar, 
-					this.barStyle, 
-					this.barAnimationStyle()
-				]}>
-					<Text style={[Style.min, this.minStyle]}>{`${min.toFixed(decimalPlaces)}${units}`}</Text>
-					<Text style={[Style.max, this.maxStyle]}>{`${max.toFixed(decimalPlaces)}${units}`}</Text>
-					{this.renderValue()}
-					<View {...this.panResponder.panHandlers} style={Style.touchableArea} />
-				</Animated.View>
-			</View>
-		);
-  	}
+    if (left <= borderRadius) {
+      left = borderRadius;
+    }
+
+    if (left >= width - borderRadius - height) {
+      left = width - borderRadius - height;
+    }
+
+    return (
+      <Animated.View
+        style={[
+          Style.valueContainer,
+          {
+            left,
+            transform: [{ scale: animScale }, { rotate: "-90deg" }],
+          },
+        ]}
+      >
+        <Animated.View
+          style={[
+            Style.valueOuterContainer,
+            this.valueOuterContainerStyle,
+            {
+              transform: [{ translateY: animTranslateY }],
+              backgroundColor: this.getCurrentColor(),
+            },
+          ]}
+        >
+          <View
+            style={[Style.valueInnerContainer, this.valueInnerContainerStyle]}
+          >
+            <Animated.Text
+              style={[
+                Style.value,
+                this.valueStyle,
+                { color: this.getCurrentColor() },
+              ]}
+              numberOfLines={1}
+            >{`${value}${units}`}</Animated.Text>
+          </View>
+        </Animated.View>
+      </Animated.View>
+    );
+  }
+
+  render() {
+    let { min, max, decimalPlaces, units } = this.props;
+
+    return (
+      <View
+        style={{
+          transform: [{ rotate: "90deg" }],
+        }}
+      >
+        {this.renderTitle()}
+        <Animated.View
+          style={[Style.bar, this.barStyle, this.barAnimationStyle()]}
+        >
+          <Text
+            style={[
+              Style.min,
+              this.minStyle,
+              { transform: [{ rotate: "-90deg" }] },
+            ]}
+          >{`${min.toFixed(decimalPlaces)}${units}`}</Text>
+          <Text
+            style={[
+              Style.max,
+              this.maxStyle,
+              { transform: [{ rotate: "-90deg" }] },
+            ]}
+          >{`${max.toFixed(decimalPlaces)}${units}`}</Text>
+          {this.renderValue()}
+          <View
+            {...this.panResponder.panHandlers}
+            style={Style.touchableArea}
+          />
+        </Animated.View>
+      </View>
+    );
+  }
 }
 
 export default Slider;
